bbb6d0f30a8cb35fc7b58c0db9683b03ae1d8ca7
unknown <ilyamarkov10a@gmail.com>
2024-05-27 18:15:55 +0500

    ышч

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const video = document.getElementById('video');
     const canvas = document.getElementById('canvas');
     const ctx = canvas.getContext('2d');
-    const video = document.createElement('video');
 
-    canvas.width = canvas.clientWidth;
-    canvas.height = canvas.clientHeight;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
         navigator.mediaDevices.getUserMedia({ video: true })
@@ -14,18 +14,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
             video.addEventListener('play', () => {
                 function drawFrame() {
+                    ctx.clearRect(0, 0, canvas.width, canvas.height);
                     ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-
-                    // Применение эффекта воды
-                    ctx.globalAlpha = 0.6;
-                    ctx.filter = 'blur(5px)';
-                    ctx.drawImage(canvas, 0, 5, canvas.width, canvas.height - 10);
-                    ctx.filter = 'none';
-                    ctx.globalAlpha = 1.0;
 
                     requestAnimationFrame(drawFrame);
                 }
-
                 drawFrame();
             });
         })
