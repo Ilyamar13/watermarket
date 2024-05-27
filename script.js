a62cc0d8e44f8ce161f1ae7fab29dfd8b251bfa4
unknown <ilyamarkov10a@gmail.com>
2024-05-27 18:01:30 +0500

    four

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const video = document.getElementById('video');
+document.addEventListener('DOMContentLoaded', () => {
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+    const video = document.createElement('video');
+
+    canvas.width = canvas.clientWidth;
+    canvas.height = canvas.clientHeight;
 
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
         navigator.mediaDevices.getUserMedia({ video: true })
@@ -13,4 +18,21 @@ document.addEventListener('DOMContentLoaded', (event) => {
     } else {
         alert("Веб-камера не поддерживается вашим устройством");
     }
+
+    video.addEventListener('play', () => {
+        function drawFrame() {
+            ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+            // Применение эффекта воды
+            ctx.globalAlpha = 0.6;
+            ctx.filter = 'blur(5px)';
+            ctx.drawImage(canvas, 0, 5, canvas.width, canvas.height - 10);
+            ctx.filter = 'none';
+            ctx.globalAlpha = 1.0;
+
+            requestAnimationFrame(drawFrame);
+        }
+
+        drawFrame();
+    });
 });
